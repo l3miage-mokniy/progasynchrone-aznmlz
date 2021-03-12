b2c5cb8f13543da73a9a37bb6d891658f8fa0a37
l3miage-mokniy <yanis.mokni@gmail.com>
2021-03-12 09:09:44 +0100

    Réalisation exo 2 + cbPromesse.ts

--- exo2.ts
+++ exo2.ts
@@ -1,10 +1,24 @@
-import {searchImagesP, getDataP, composeMozaicP, sendToServerP} from "./cbPromesse";
-import {log} from "./cbAPI";
-
+import {
+  searchImagesP,
+  getDataP,
+  composeMozaicP,
+  sendToServerP
+} from "./cbPromesse";
+import { log } from "./cbAPI";
 
 function exo2(description: string) {
+  searchImagesP(description)
+    .then(urls => Promise.all(urls.map(getDataP)))
+    .then(
+      //images => composeMozaicP(images)
+      //equivalent
+      composeMozaicP
+    )
+    .then(
+      //img => sendToServerP(img)
+      sendToServerP
+    );
 }
 
-
-const bt2 = document.querySelector('#bt2') as HTMLElement;
-bt2.onclick = () => exo2('chats');
+const bt2 = document.querySelector("#bt2") as HTMLElement;
+bt2.onclick = () => exo2("chats");
