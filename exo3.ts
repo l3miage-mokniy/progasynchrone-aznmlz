b5c5de6e6fbcf2bc3e340aa5c50c780153e71c94
l3miage-mokniy <yanis.mokni@gmail.com>
2021-03-12 09:23:12 +0100

    exo3 complet

--- exo3.ts
+++ exo3.ts
@@ -1,12 +1,21 @@
-import {searchImagesP, getDataP, composeMozaicP, sendToServerP} from "./cbPromesse";
-import {log} from "./cbAPI";
+import {
+  searchImagesP,
+  getDataP,
+  composeMozaicP,
+  sendToServerP
+} from "./cbPromesse";
+import { log } from "./cbAPI";
 
 async function exo3(descr: string): Promise<void> {
+  const urls = await searchImagesP(descr);
+  const images = await Promise.all(urls.map(getDataP));
+  const imgM = await composeMozaicP(images);
+  await sendToServerP(imgM);
 }
 
-const bt3 = document.querySelector('#bt3') as HTMLElement;
+const bt3 = document.querySelector("#bt3") as HTMLElement;
 bt3.onclick = () => {
   log("avant l'appelle à exo3");
-  exo3('chats');
+  exo3("chats");
   log("après l'appelle à exo3");
 };
